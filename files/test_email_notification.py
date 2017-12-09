be4df612cb64b67f5804dddb8bf82d27e716713a
masterhui <masterhui@users.noreply.github.com>
2017-12-09 02:01:21 +0100

    Update and rename test_sonar.py to test_email_notification.py
    Add sonar sensor test code

---
+++ files/test_email_notification.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import yagmail
+import argparse
+
+AUTH_PATH = "/root/auth.yagmail"
+
+
+def main(args):
+       
+    # Read credentials
+    f = open(AUTH_PATH, 'r')
+    user = f.readline().replace('\n', '')
+    pw = f.readline().replace('\n', '')
+    #print 'user=%s pw=%s' %(user, pw)
+
+    # Register connection
+    yag = yagmail.SMTP(user, pw)    
+    
+    # Send the email
+    yag.send(to = args.to, subject = args.subject, contents = args.body)     
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='GreenPiThumb Email Notification Diagnostic Test',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '-t',
+        '--to',
+        help='Email recipient')
+    parser.add_argument(
+        '-s',
+        '--subject',
+        help='Email subject', default="GreenPiThumb Email Notification Test")
+    parser.add_argument(
+        '-b',
+        '--body',
+        help='Email body', default="")     
+    parser.add_argument(
+        '-u',
+        '--username',
+        help='The gmail user name for sending the email')                 
+        
+    main(parser.parse_args()) 
