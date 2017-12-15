37a678a4d34207b032220b12c8294b0e9acc11f7
masterhui <masterhui@users.noreply.github.com>
2017-12-15 23:43:50 +0100

    Create test_dht22.py
    Add dht22 test

---
+++ files/test_dht22.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import argparse
+
+import Adafruit_DHT
+
+
+def main(args):
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22,
+                                                    args.dht_pin)
+    if humidity is not None and temperature is not None:
+        print(
+            'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='GreenPiThumb DHT22 Diagnostic Test',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '-p',
+        '--dht_pin',
+        type=int,
+        help='GPIO pin that DHT is plugged in to',
+        default=21)
+    main(parser.parse_args())
